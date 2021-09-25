0ce22db9f4ea623b28813011a2104d8b5f1d12a4
rajal2002 <91373239+rajal2002@users.noreply.github.com>
2021-09-25 16:44:04 +0530

    Add files via upload

---
+++ staff/www.elegantthemes.com/layouts/education/elementary-school-staff-page/c1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int  main(int argc, char const *argv[])
+{
+	int a,b;
+	printf("enter the number a\n");
+	scanf("%d",&a);
+	printf("enter a number b\n");
+	scanf("%d",&b);
+	printf("enter the sum %d\n",a-b);
+	sum=a-b
+	return 0;
+}
